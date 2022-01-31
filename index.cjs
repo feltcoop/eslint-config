fb884e6178e4f110d938d1bc368f59724c155ce5
Ryan Atkinson <mail@ryanatkn.com>
2022-01-31 12:21:31 -0600

    add config to get svelte and typescript working

--- index.cjs
+++ index.cjs
@@ -4,12 +4,22 @@ module.exports = {
 		node: true,
 		es2021: true,
 	},
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 'latest',
 		sourceType: 'module',
 	},
 	plugins: ['@typescript-eslint', 'svelte3'],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			processor: 'svelte3/svelte3',
+		},
+	],
+	settings: {
+		'svelte3/typescript': true,
+	},
 	rules: {
 		'no-var': 1,
 	},
